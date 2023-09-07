40120261eb5688085ab5a567021b1655de5a5c97
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-09-07 04:13:40 -0700

    Add files via upload

---
+++ find size of data type.cpp
@@ -0,0 +1,21 @@
+   /*
+   @Title: Program to find the size of data type.
+   @Written by: Noor Fatima.
+   */
+   
+   #include<iostream>
+   using namespace std;
+   int main()
+   
+   {
+   	cout<<"the size of int is="<< sizeof(int)<<"bytes"<<endl;
+   	cout<<"the size of char is="<< sizeof(char)<<"bytes"<<endl;
+   	cout<<"the size of string is="<< sizeof(string)<<"bytes"<<endl;
+   	cout<<"the size of float is="<< sizeof(float)<<"bytes"<<endl;
+   	cout<<"the size of bool is="<< sizeof(bool)<<"bytes"<<endl;
+   	cout<<"the size of double is=0"<< sizeof(double)<<"bytes"<<endl;
+   	
+   	return 0;
+	   
+	      }
+   
